0495e2c7298a54092cbf300e41d63d18d5a5394a
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2023-02-02 18:01:59 +0530

    Removed Home.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Notes from "./components/Notes";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import About from "./components/About";
@@ -31,7 +32,7 @@ function App() {
           <Alert alert={alert}  />
           <div className="container">
             <Routes>
-              <Route path="/" element={<Home showAlert={showAlert}/>} ></Route>
+              <Route path="/" element={<Notes showAlert={showAlert}/>} ></Route>
 
               <Route path="/about" element={<About />}></Route>
               <Route
