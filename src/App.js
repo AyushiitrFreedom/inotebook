4e3e11fc7629154b85442ec4dbe551a993d99013
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2023-02-02 21:50:56 +0530

    removed home from appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 import Notes from "./components/Notes";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
